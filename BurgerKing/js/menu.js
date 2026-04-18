84e3da708ce2945ecb6a4c2cc9f8e8ce2aa12acb
Emely Saravia <emelysrv149@gmail.com>
2026-04-18 01:27:11 -0600

    Delete BurgerKing directory

--- BurgerKing/js/menu.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const hamburgerIcon = document.querySelector('.hamburger-icon');
-    const menuList = document.getElementById('menuList');
-    
-    hamburgerIcon.classList.toggle('active');
-    menuList.classList.toggle('active');
-}
